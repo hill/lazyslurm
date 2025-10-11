155bebe0d78b495995e25a7d2fb7808a26d5a6a6
Francis Rossi <francisdrossi@gmail.com>
2025-10-10 21:33:29 -0400

    Added filter for partitions and cleaned up some redundant code
    Signed-off-by: Francis Rossi <francisdrossi@gmail.com>
    Change-Id: I46184a52727e16407a6011dc6f0221af6a6a6964

--- src/ui/events.rs
+++ src/ui/events.rs
@@ -13,7 +13,39 @@ pub async fn handle_key_event(app: &mut App, key: KeyEvent) -> Result<Option<()>
         AppState::Normal => event_normal_state(app, key).await,
         AppState::UserSearchPopup => event_user_search_popup(app, key).await,
         AppState::CancelJobPopup => event_cancel_popup(app, key).await,
+        AppState::PartitionSearchPopup => event_partition_search_popup(app, key).await,
     }
+}
+
+pub async fn handle_text_event(app: &mut App, key: KeyEvent) -> Option<Option<String>> {
+    match key.code {
+        KeyCode::Enter => {
+            if app.input.is_empty() {
+                return Some(None);
+            } else {
+                return Some(Some(app.input.clone()));
+            }
+        }
+        KeyCode::Esc => {
+            app.input.clear();
+            app.state = AppState::Normal;
+        }
+        KeyCode::Char(c) => {
+            app.input.push(c);
+        }
+        KeyCode::Backspace => {
+            app.input.pop();
+        }
+        _ => {}
+    }
+    None
+}
+
+pub async fn reset_popup_state_to_normal(app: &mut App) -> Result<(), Box<dyn Error>> {
+    app.input.clear();
+    app.state = AppState::Normal;
+    app.refresh_jobs().await?;
+    Ok(())
 }
 
 async fn event_normal_state(app: &mut App, key: KeyEvent) -> Result<Option<()>, Box<dyn Error>> {
@@ -35,6 +67,9 @@ async fn event_normal_state(app: &mut App, key: KeyEvent) -> Result<Option<()>, 
         (KeyCode::Char('u'), _) => {
             app.state = AppState::UserSearchPopup;
         }
+        (KeyCode::Char('p'), _) => {
+            app.state = AppState::PartitionSearchPopup;
+        }
         (KeyCode::Char('c'), _) if app.selected_job.is_some() => {
             app.confirm_action = false;
             app.state = AppState::CancelJobPopup;
@@ -48,30 +83,22 @@ async fn event_user_search_popup(
     app: &mut App,
     key: KeyEvent,
 ) -> Result<Option<()>, Box<dyn Error>> {
-    match key.code {
-        KeyCode::Enter => {
-            if app.input.is_empty() {
-                app.current_user = None;
-            } else {
-                app.current_user = Some(app.input.clone());
-            }
+    let user_search = handle_text_event(app, key).await;
+    if let Some(user) = user_search {
+        app.current_user = user;
+        reset_popup_state_to_normal(app).await?;
+    }
+    Ok(None)
+}
 
-            app.input.clear();
-            app.state = AppState::Normal;
-
-            app.refresh_jobs().await?;
-        }
-        KeyCode::Esc => {
-            app.input.clear();
-            app.state = AppState::Normal;
-        }
-        KeyCode::Char(c) => {
-            app.input.push(c);
-        }
-        KeyCode::Backspace => {
-            app.input.pop();
-        }
-        _ => {}
+async fn event_partition_search_popup(
+    app: &mut App,
+    key: KeyEvent,
+) -> Result<Option<()>, Box<dyn Error>> {
+    let partition_search = handle_text_event(app, key).await;
+    if let Some(partition) = partition_search {
+        app.current_partition = partition;
+        reset_popup_state_to_normal(app).await?;
     }
     Ok(None)
 }
